5c16ca7d637dcc63c2fe1bfba8b306ac382fc6a2
AndreyStetsenko <andrey@stetsenko.org>
2022-06-21 12:00:47 +0300

    Update post

--- index.js
+++ index.js
@@ -23,58 +23,56 @@ await doc.useServiceAccountAuth({
 })
 
 app.post('/new-message', async (req, res) => {
+  const { message } = req.body
 
-})
-
-const { message } = req.body
-
-const messageText = message?.text?.toLowerCase()?.trim()
-const chatId = message?.chat?.id
-if (!messageText || !chatId) {
-  return res.sendStatus(400)
-}
-
-await doc.loadInfo()
-const sheet = doc.sheetsByIndex[0]
-const rows = await sheet.getRows()
-const dataFromSpreadsheet = rows.reduce((obj, row) => {
-  if (row.date) {
-    const todo = {
-      text: row.text,
-      done: row.done
-    }
-
-    obj[row.date] = obj[row.date] ? [...obj[row.date], todo] : [todo]
+  const messageText = message?.text?.toLowerCase()?.trim()
+  const chatId = message?.chat?.id
+  if (!messageText || !chatId) {
+    return res.sendStatus(400)
   }
-  return obj
-}, {})
-
-let responseText = 'I have nothink to say.'
-
-if (messageText === 'joke') {
+  
+  await doc.loadInfo()
+  const sheet = doc.sheetsByIndex[0]
+  const rows = await sheet.getRows()
+  const dataFromSpreadsheet = rows.reduce((obj, row) => {
+    if (row.date) {
+      const todo = {
+        text: row.text,
+        done: row.done
+      }
+  
+      obj[row.date] = obj[row.date] ? [...obj[row.date], todo] : [todo]
+    }
+    return obj
+  }, {})
+  
+  let responseText = 'I have nothink to say.'
+  
+  if (messageText === 'joke') {
+    try {
+      const response = await axios(JOKE_API)
+      responseText = response.data.joke
+    } catch (e) {
+      console.log(e)
+      res.send(e)
+    }
+  } else if (/\d\d\.\d\d/.test(messageText)) {
+    responseText = dataFromSpreadsheet[messageText] || 'Have you nothing to do on this day.'
+  }
+  
   try {
-    const response = await axios(JOKE_API)
-    responseText = response.data.joke
+    await axios.post(TELEGRAM_URI, {
+      chat_id: chatId,
+      text: responseText
+    })
+    res.send('Done')
   } catch (e) {
     console.log(e)
     res.send(e)
   }
-} else if (/\d\d\.\d\d/.test(messageText)) {
-  responseText = dataFromSpreadsheet[messageText] || 'Have you nothing to do on this day.'
-}
-
-try {
-  await axios.post(TELEGRAM_URI, {
-    chat_id: chatId,
-    text: responseText
+  
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
   })
-  res.send('Done')
-} catch (e) {
-  console.log(e)
-  res.send(e)
-}
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
 })
